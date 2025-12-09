24b3d185f6292b06541f61e71f02a316ad3de26d
bbruno123 <bruno60835396@aluno.es.senac.br>
2025-12-09 20:58:34 +0000

    update

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-const titulo = document.querySelector('h1')
-console.log(titulo)
+const botaoMenu = document.querySelector("#botao_menu");
+const menuEsquerdo = document.querySelector("#menu_esquerdo");
+const menuEsquerdoBotao = document.querySelector("#menu_esquerdo_botao");
+
+function ativarMenu() {
+    if (!botaoMenu.classList.contains("hidden")) {
+
+        botaoMenu.classList.add("hidden");
+        menuEsquerdo.classList.remove("hidden");
+        menuEsquerdo.classList.add("show");
+    }
+}
+
+
+function fecharMenu() {
+    if (!menuEsquerdo.classList.contains("hidden")) {
+        
+        menuEsquerdo.classList.remove("show");
+        menuEsquerdo.classList.add("hidden");
+        botaoMenu.classList.remove("hidden");
+
+    }
+}
+
+botaoMenu.addEventListener('click', ativarMenu);
+menuEsquerdoBotao.addEventListener('click', fecharMenu);
