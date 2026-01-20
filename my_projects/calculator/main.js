1245f58569a97b238ed8f5764c3217545bf9236f
bbruno123 <bruno60835396@aluno.es.senac.br>
2026-01-20 18:57:05 +0000

    update

--- my_projects/calculator/main.js
+++ my_projects/calculator/main.js
@@ -1,36 +1,118 @@
+let num1 = "";
+let num2 = "";
+
+let plus = false;
+
 const display = document.getElementById("display");
 
+console.log(Number("445") + Number("555"));
+
 window.addEventListener("keydown", (event) => {
+    if (plus === false){
+
+        if (event.key === "0" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "1" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "2" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "3" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "4" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "5" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "6" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "7" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "8" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+        if (event.key === "9" && !event.repeat){
+            num1 = event.key;
+            display.value += num1;
+        }
+    }
+
+    if (event.key === "+" && !event.repeat){
+        plus = true;
+        display.value = "";
+    }
+
+    if (plus === true){
     
-    if (event.key === "0" && !event.repeat){
-        display.value += "0";
-    }
-    if (event.key === "1" && !event.repeat){
-        display.value += "1";
-    }
-    if (event.key === "2" && !event.repeat){
-        display.value += "2";
-    }
-    if (event.key === "3" && !event.repeat){
-        display.value += "3";
-    }
-    if (event.key === "4" && !event.repeat){
-        display.value += "4";
-    }
-    if (event.key === "5" && !event.repeat){
-        display.value += "5";
-    }
-    if (event.key === "6" && !event.repeat){
-        display.value += "6";
-    }
-    if (event.key === "7" && !event.repeat){
-        display.value += "7";
-    }
-    if (event.key === "8" && !event.repeat){
-        display.value += "8";
-    }
-    if (event.key === "9" && !event.repeat){
-        display.value += "9";
+        if (event.key === "0" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "1" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "2" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "3" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "4" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "5" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "6" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "7" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "8" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
+        if (event.key === "9" && !event.repeat){
+            num2 = event.key;
+            display.value += num2;
+        }
     }
     
+    if (event.key === "Enter" && !event.repeat){
+        display.value = Number(num1) + Number(num2);
+        num1 = "";
+        num2 = "";
+        // plus = false;
+    }
+
+    if (event.key === "Backspace" && !event.repeat){
+        display.value = "";
+        num1 = "";
+        num2 = "";
+        // plus = false;
+    }
+
 });
