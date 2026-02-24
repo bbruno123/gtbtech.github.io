f3563b94f4e371a81e1b28e3eaef545b63e9d753
AMGOIABEIRA06 <AMGOIABEIRA06@AMPLAMOVEIS.INTRANET>
2026-02-24 18:17:43 -0300

    update

--- my_projects/python_snake_game/snake.py
+++ my_projects/python_snake_game/snake.py
@@ -1,20 +1,63 @@
 import os
 import time
 # import keyboard
-from pynput.keyboard import Key, Controller
+from pynput import keyboard
 
-keyboard = Controller()
-keyboard.type("ola")
+grid = []
 
+for i in range(7):
+    row = []
+    for j in range(7):
+        row.append("")
+    grid.append(row)
 
-# print("oi")
-# os.system("clear")
+def display():
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[H\033[J", end="")  # limpa rápido
+    # time.sleep(0.2)
 
-# i0 = 0
+    for i in range(len(grid)):
+        print(grid[i])
 
-# while True:
-#     if keyboard.is_pressed('a'):
-#         print("oi")
+key_releasead = False
 
-#     time.sleep(0.1)
-#     # i0 += 1
+pressed_keys = set()
+
+def key_press(k):
+
+    if k in pressed_keys:
+        return
+    
+    pressed_keys.add(k)
+
+    try:
+        if k.char == 'w':
+            print("up")
+        elif k.char == 's':
+            print("down")
+        elif k.char == 'a':
+            print("left")
+        elif k.char == 'd':
+            print("right")
+
+    except AttributeError:
+        pass
+
+def key_release(k):
+    display()
+    pressed_keys.discard(k)
+    
+    if k == keyboard.Key.esc:
+        # print(len(grid))
+        print("Limpando grid antes de sair...")
+        grid.clear()
+        return False
+
+try:
+    with keyboard.Listener(on_press=key_press, on_release=key_release) as listener:
+        listener.join()
+
+finally:
+    print("Limpando grid antes de sair...")
+    grid.clear()
+
