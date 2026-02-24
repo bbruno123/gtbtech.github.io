072c64a1e913ccefd8928c3846d1aa2b813a565e
bbruno123 <bruno60835396@aluno.es.senac.br>
2026-02-24 14:57:01 +0000

    update

---
+++ my_projects/python_snake_game/snake.py
@@ -0,0 +1,20 @@
+import os
+import time
+# import keyboard
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+keyboard.type("ola")
+
+
+# print("oi")
+# os.system("clear")
+
+# i0 = 0
+
+# while True:
+#     if keyboard.is_pressed('a'):
+#         print("oi")
+
+#     time.sleep(0.1)
+#     # i0 += 1
